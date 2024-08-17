5132d2bda1c1842ad71a7a9664229c5018872861
campell <120425425+CamPells@users.noreply.github.com>
2024-08-17 18:26:04 +1200

    adde dataStore

---
+++ app/src/main/java/cpe81/flashcard/app/datastore/DataAccessModule.kt
@@ -0,0 +1,32 @@
+package cpe81.flashcard.app.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.FlowPreview
+import cpe81.flashcard.app.models.Note
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "nz.ac.canterbury.seng303.lab1.shared.preferences")
+
+@FlowPreview
+val dataAccessModule = module {
+    single<Storage<Note>> {
+        PersistentStorage(
+            gson = get(),
+            type = object : TypeToken<List<Note>>() {}.type,
+            preferenceKey = stringPreferencesKey("notes"),
+            dataStore = androidContext().dataStore
+        )
+    }
+
+    single { Gson() }
+
+}
